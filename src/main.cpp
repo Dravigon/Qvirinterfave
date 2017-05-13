f16170001046af92bc071f6c29f50f4d1b878182
dravigon <dravigon@github.com>
2017-05-13 23:32:16 +0530

    minor corrections

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
     engine.addImportPath("/home/dravigon/qt/Qvirinterface" );
 
     //create the qml instance
-    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+    engine.load(QUrl(QLatin1String("qrc:/qml/main.qml")));
 
     return app.exec();
 }
